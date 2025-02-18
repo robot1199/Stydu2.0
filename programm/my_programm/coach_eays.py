1c2f414c7039a6066b75151bc18bf023a05f0133
Alex <robotalex1904@gmail.com>
2025-02-18 13:35:48 +0300

    video1

--- programm/my_programm/coach_eays.py
+++ programm/my_programm/coach_eays.py
@@ -17,31 +17,36 @@ cap.release()
 current_frame = 0
 playback_speed = 100 # задержка в милисекундах(100 мс = нормальная скорость)
 is_playing = True
-
+print(playback_speed)
 
 while True:
     if is_playing:
         # Отображение текущего кадра
         cv2.imshow('Video', frames[current_frame])
-        current_frame = (current_frame + 1) % len((frames)) # циклический переход к началу
+        current_frame = (current_frame + 1) % len(frames)  # Циклический переход к началу
 
-    key = cv2.waitKey(playback_speed)    # Задержка для воспроизведения
-    if key & 0xFF == ord('q'):     # Выход
+    key = cv2.waitKey(playback_speed)  # Задержка для воспроизведения
+
+    if key & 0xFF == ord('q'):  # Выход
         break
-    elif key & 0xFF == ord('s'): # Остановка
-        is_playing = not is_playing
+    elif key & 0xFF == ord('s'):  # Остановка
+        is_playing = not is_playing  # Переключение состояния воспроизведения
         print('stop/go')
-    elif key & 0xFF == ord('b'): # возврат на один кадр
+    elif key & 0xFF == ord('b'):  # Возврат на один кадр
         current_frame = max(0, current_frame - 1)
-    elif key & 0xFF == ord('f'): # вперед на один кадр
-        current_frame = min(len(frames) -1, current_frame - 1)
-        print(playback_speed)
-    elif key & 0xFF == ord('+'): # Ускорение
-        playback_speed = max(1, playback_speed - 10) # Ускорение задержки
-        print(playback_speed)
-    elif key & 0xFF == ord('-'): # Замедление
-        playback_speed += 10 # Увиличение задкржки
+        print('recovery')
+    elif key & 0xFF == ord('f'):  # Вперед на один кадр
+        current_frame = min(len(frames) - 1, current_frame + 1)
+        print('plus one steep')
+    elif key & 0xFF == ord('+'):  # Ускорение
+        playback_speed = max(1, playback_speed - 10)  # Уменьшение задержки
+        print(f'acceleration playback_speed = {playback_speed}')
+    elif key & 0xFF == ord('-'):  # Замедление
+        playback_speed += 10  # Увеличение задержки
+        print(f'deceleration playback_speed = {playback_speed}')
 
+    if not is_playing:
+        cv2.imshow('Video', frames[current_frame])
 
 
 
